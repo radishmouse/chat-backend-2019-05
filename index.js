a02b7ed79987165e8fac774ac03a5b3bcd744c3d
chris aquino <chris@radishmouse.com>
2019-05-02 14:06:26 -0400

    echoes in the post

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 
 const express = require('express');
 const app = express();
+app.use(express.urlencoded({extended: true}));
+
+// This is my "database"
+const db = [
+    'Welcome to the chat app!'
+];
+
+// When GET request comes in,
+// send back all the messages.
+app.get('/', (req, res) => {
+    res.json(db);
+});
+
+// When POST request comes in,
+// add message to array of messages.
+app.post('/', (req, res) => {
+    // what do we do here?
+    console.log(req.body.message);
+    res.json({
+        'message': req.body.message
+    })
+});
 
 app.listen(31337, () => {
     console.log(`You're cooking with gasoline!`);
